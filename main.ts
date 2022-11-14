e8a7f225340a6371e7010ea605d6b63c2977aad3
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2022-11-14 12:51:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,13 @@
 let hand = 0
+/**
+ * Extensions
+ * 
+ * 1. clear screen
+ * 
+ * 2. animate the scissors
+ * 
+ * 3. tilt controls to choose
+ */
 // ExtensionsL
 // 
 // button A+B- clear screen
@@ -6,7 +15,7 @@ let hand = 0
 // button A - keep score (you)
 // 
 // button B - keep score (other)
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.A, function () {
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
@@ -23,5 +32,19 @@ input.onGesture(Gesture.Shake, function () {
         basic.showIcon(IconNames.Square)
     } else if (hand == 3) {
         basic.showIcon(IconNames.Scissors)
+    }
+})
+basic.forever(function () {
+    if (hand == 3) {
+        basic.showIcon(IconNames.Scissors)
+        basic.pause(100)
+        basic.showLeds(`
+            # # . . .
+            # # . . .
+            . . # # #
+            # # . . .
+            # # . . .
+            `)
+        basic.pause(100)
     }
 })
